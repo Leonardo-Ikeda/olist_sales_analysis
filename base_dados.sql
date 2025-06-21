30f6d4d51bf5ac018c4e7620ca8094d5436ed98f
Leonardo Ikeda <leoikeda@gmail.com>
2025-06-21 11:20:28 -0300

    Update base_dados.sql

--- base_dados.sql
+++ base_dados.sql
@@ -6,14 +6,6 @@ CREATE TABLE customers (
 	customer_state CHAR(2)
 );
 
-CREATE TABLE geolocation (
-	geolocation_zip_code_prefix VARCHAR(255),
-	geolocation_lat REAL,
-	geolocation_lng REAL,
-	geolocation_city VARCHAR(255),
-	geolocation_state CHAR(2)	
-);
-
 CREATE TABLE order_items (
 	order_id VARCHAR(255),
 	order_item_id INTEGER,
@@ -21,36 +13,20 @@ CREATE TABLE order_items (
 	seller_id VARCHAR(255),
 	shipping_limit_date TIMESTAMP,
 	price DECIMAL(10,2),
-	freight_value DECIMAL(10,2)
-);
-
-CREATE TABLE order_payments (
-	order_id VARCHAR(255),
-	payment_sequential INTEGER,
-	payment_type VARCHAR(255),
-	payment_installment INTEGER,
-	payment_value DECIMAL(10,2)
-)
-
-CREATE TABLE order_reviews (
-	review_id VARCHAR(255),
-	order_id VARCHAR(255),
-	review_score INTEGER,
-	review_comment_title TEXT,
-	review_comment_message TEXT,
-	review_creation_date TIMESTAMP,
-	review_answer_timestamp TIMESTAMP
+	freight_value DECIMAL(10,2),
+	FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 CREATE TABLE orders (
-	order_id VARCHAR(255),
+	order_id VARCHAR(255) PRIMARY KEY,
 	customer_id VARCHAR(255),
 	order_status VARCHAR(100),
 	order_purchase_timestamp TIMESTAMP,
 	order_approved_at TIMESTAMP,
 	order_delivered_carrier_date TIMESTAMP,
 	order_delivered_customer_date TIMESTAMP,
-	order_estimated_delivery_date TIMESTAMP
+	order_estimated_delivery_date TIMESTAMP,
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE products (
@@ -63,11 +39,4 @@ CREATE TABLE products (
 	product_length_cm INTEGER,
 	product_height_cm INTEGER,
 	product_width_cm INTEGER
-);
-
-CREATE TABLE sellers (
-	seller_id VARCHAR(255) PRIMARY KEY,
-	seller_zip_code_prefix CHAR(5),
-	seller_city VARCHAR(255),
-	seller_state CHAR(2)
 );
